fe4c5e56c9ec11fe8537716b751bcfd693723c28
MariumIrfan2 <111229052+MariumIrfan2@users.noreply.github.com>
2022-09-28 16:21:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function squareShape() {
+    var shapes = document.getElementById("shapes");
+    console.log(shapes);
+    shapes.className = "square"
+
+}
+function rectangleShape() {
+    var shapes = document.getElementById("shapes");
+    console.log(shapes);
+    shapes.className = "rectangle"
+
+}
+function circleShape() {
+    var shapes = document.getElementById("shapes");
+    console.log(shapes);
+    shapes.className = "circle"
+
+}
