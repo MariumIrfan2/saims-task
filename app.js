255ad8f70a180614c094273ebe11fd0f1b929c50
MariumIrfan2 <111229052+MariumIrfan2@users.noreply.github.com>
2022-10-07 15:50:26 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,102 @@
-function squareShape() {
-    var shapes = document.getElementById("shapes");
-    console.log(shapes);
-    shapes.className = "square"
+var img = document.getElementById("img");
+
+// var imgArray = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
+// var i = 0;
+
+
+// function previous() {
+//     for (i = 0; i < imgArray.lenght; i++) {
+//         i = imgArray[i];
+//     }
+// }
+
+
+function prev(id,src){
+    img.src = src;
 
 }
-function rectangleShape() {
-    var shapes = document.getElementById("shapes");
-    console.log(shapes);
-    shapes.className = "rectangle"
 
-}
-function circleShape() {
-    var shapes = document.getElementById("shapes");
-    console.log(shapes);
-    shapes.className = "circle"
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function previous() {
+//     if (i <= 0) {
+//         i++;
+//         i= imgArray[i];
+
+//         // return otherImgs();
+
+//     }
+// }
+
+
+
+
+// function next() {
+//     if (i >= images.lenght - 1) {
+//         i = -1;
+//         i++;
+
+//         return otherImgs()
+//     }
+
+// }
+
+// function otherImgs(i) {
+//     return img;
+// }
+
+
+// function slideshow() {
+//     var slides = document.getElementsByClassName("slides");
+
+//     slides[num].getElementsByClassName.display = block;
+
+// }
+
+// var i = 0;
+// function control(x) {
+
+//     i = i + x;
+
+// }
+
+
+
+// var slides = document.getElementsByClassName("slides")
+
+
+// function changeImage() {
+//     console.log("slides");
+
+//     for (i = 0; i < images.length; i++) {
+
+
+//     }
+// }
